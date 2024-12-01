6c1618bc64921c3194c104db87105b873eff8e04
Hippolyte lefer <hippolytelefer@gmail.com>
2024-12-01 18:51:53 +0100

    feat: aoc day 1/part1 in c

---
+++ day1/day1.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int left;
+    int right;
+    struct Node *next;
+} Node;
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int calculate_total_distance(int *left, int *right, int size) {
+    int total_distance = 0;
+    for (int i = 0; i < size; i++) {
+        total_distance += abs(left[i] - right[i]);
+    }
+    return total_distance;
+}
+
+int get_input(int **left, int **right) {
+    char *line = NULL;
+    size_t len = 0;
+    int size = 0;
+    Node *head = NULL, *tail = NULL;
+
+    while (getline(&line, &len, stdin) != -1) {
+        Node *new_node = (Node*)malloc(sizeof(Node));
+        sscanf(line, "%d %d", &new_node->left, &new_node->right);
+        new_node->next = NULL;
+        if (tail) {
+            tail->next = new_node;
+        } else {
+            head = new_node;
+        }
+        tail = new_node;
+        size++;
+    }
+    free(line);
+
+    *left = (int*)malloc(size * sizeof(int));
+    *right = (int*)malloc(size * sizeof(int));
+
+    Node *current = head;
+    for (int i = 0; i < size; i++) {
+        (*left)[i] = current->left;
+        (*right)[i] = current->right;
+        Node *temp = current;
+        current = current->next;
+        free(temp);
+    }
+
+    return size;
+}
+
+int main() {
+    int *left = NULL;
+    int *right = NULL;
+
+    int size = get_input(&left, &right);
+
+    qsort(left, size, sizeof(int), compare);
+    qsort(right, size, sizeof(int), compare);
+
+    printf("Total distance: %d\n", calculate_total_distance(left, right, size));
+
+    free(left);
+    free(right);
+    return 0;
+}

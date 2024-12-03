de44532c0c0f48fd5452a14f2eb58b7941d54e43
Hippolyte lefer <hippolytelefer@gmail.com>
2024-12-03 06:08:13 +0100

    feat: day 3/part 2 in python

--- day3/day3.py
+++ day3/day3.py
@@ -2,21 +2,27 @@ import sys
 import re
 
 def solve(data):
-    pattern = r'mul\((\d{1,3}),(\d{1,3})\)'
+    pattern = r'(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don\'t\(\))'
     matches = re.findall(pattern, data)
-
+    
     total = 0
+    enabled = True
+
     for match in matches:
-        x, y = map(int, match)
-        total += x * y
+        if match[1] and match[2]:
+            if enabled:
+                x, y = int(match[1]), int(match[2])
+                total += x * y
+        elif match[3]:
+            enabled = True
+        elif match[4]:
+            enabled = False
 
     return total
-
 
 def main():
     data = sys.stdin.read()
     print(f"res: {solve(data)}")
-
 
 if __name__ == "__main__":
     main()

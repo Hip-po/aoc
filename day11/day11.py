a12fc9fd7a2801450bc35a876b83fe253159eac2
Hippolyte lefer <hippolytelefer@gmail.com>
2024-12-11 06:09:30 +0100

    feat: solution for day 11 in python

--- day11/day11.py
+++ day11/day11.py
@@ -1,7 +1,24 @@
 import sys
 
 def solve(data):
-    pass
+    stones = list(map(int, ''.join(data).strip().split()))
+    blinks = 75
+
+    for _ in range(blinks):
+        new_stones = []
+        for stone in stones:
+            if stone == 0:
+                new_stones.append(1)
+            elif len(str(stone)) % 2 == 0:
+                half_len = len(str(stone)) // 2
+                left_half = int(str(stone)[:half_len])
+                right_half = int(str(stone)[half_len:])
+                new_stones.extend([left_half, right_half])
+            else:
+                new_stones.append(stone * 2024)
+        stones = new_stones
+
+    return len(stones)
 
 def main():
     data = sys.stdin.readlines()
